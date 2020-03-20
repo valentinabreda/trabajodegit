c9cc71a704897fd9003d514340eb9ab44877682f
Franco-Casuscelli <franco.casuscelli5@gmail.com>
2020-03-20 20:12:47 -0300

    uniendo ambos archivos

--- valentina3.js
+++ valentina3.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 console.log('vale');
-
-
-
-
 
 let var1 = "No Quiero estar en "
 let var2 = "cuarentena"
 
 console.log(var1 + var2)
-=======
+
 let nombre = 'valentina';
 let apellido = 'breda';
 console.log(nombre + ' ' + apellido);
->>>>>>> 4e21170d4532b67941929bf782063b9adc295d41
